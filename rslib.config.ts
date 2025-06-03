daa5c119386a8508e39c13195467467bf3191386
1yasa <xiewendao@gmail.com>
2025-06-03 17:38:04 +0800

    chore: update deps

--- rslib.config.ts
+++ rslib.config.ts
@@ -1,20 +1,20 @@
-import { deepmerge } from 'deepmerge-ts'
-
 import { rslib } from '../../config'
+
+import { deepmerge } from 'deepmerge-ts'
 
 import type { RslibConfig } from '@rslib/core'
 
 const modules = ['common', 'dnd', 'emittery', 'mobx', 'react', 'storage', 'dom', 'graph', 'utils']
 
 export default deepmerge(rslib, {
-  source: {
-    entry: modules.reduce(
-      (total, item) => {
-        total[item] = `./src/${item}/index.ts`
+	source: {
+		entry: modules.reduce(
+			(total, item) => {
+				total[item] = `./src/${item}/index.ts`
 
-        return total
-      },
-      {} as Record<string, string>,
-    ),
-  },
+				return total
+			},
+			{} as Record<string, string>
+		)
+	}
 } as Partial<RslibConfig>)
