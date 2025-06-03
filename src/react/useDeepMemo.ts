d351839577e46dde4e520b5a4873488abdc579f7
1yasa <xiewendao@gmail.com>
2025-06-03 17:38:47 +0800

    chore: format

--- src/react/useDeepMemo.ts
+++ src/react/useDeepMemo.ts
@@ -2,11 +2,11 @@ import { useRef } from 'react'
 import { deepEqual } from 'fast-equals'
 
 export default <TKey, TValue>(memoFn: () => TValue, key: TKey): TValue => {
-  const ref = useRef<{ key: TKey; value: TValue }>(null)
+	const ref = useRef<{ key: TKey; value: TValue }>(null)
 
-  if (!ref.current || !deepEqual(key, ref.current.key)) {
-    ref.current = { key, value: memoFn() }
-  }
+	if (!ref.current || !deepEqual(key, ref.current.key)) {
+		ref.current = { key, value: memoFn() }
+	}
 
-  return ref.current.value
+	return ref.current.value
 }
