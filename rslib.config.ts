5c2115c2e0cae167dd69561adfeb78814ab7d7ce
1yasa <xiewendao@gmail.com>
2025-08-05 21:32:04 +0800

    fix: update build script and add dts option in rslib config

--- rslib.config.ts
+++ rslib.config.ts
@@ -7,7 +7,7 @@ import type { RslibConfig } from '@rslib/core'
 const modules = ['common', 'dnd', 'emittery', 'mobx', 'react', 'storage', 'dom', 'graph', 'utils']
 
 export default deepmerge(rslib, {
-	lib: [{ format: 'esm' }],
+	lib: [{ format: 'esm', dts: true }],
 	output: { target: 'web' },
 	source: {
 		entry: modules.reduce(
